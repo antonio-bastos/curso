becc53ef503db119aa187cc5bc79c67969f05740
Antonio Bastos <51927118+antonio-bastos@users.noreply.github.com>
2024-10-11 12:57:13 +0100

    upd

--- Modulo1_UFCD_0804/Algoritmos/Fluxogramas/a.cpp
+++
@@ -1,9 +0,0 @@
-// curso.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
-#include <iostream>
-
-int aaa()
-{
-    std::cout << "ola!\n";
-    return 0;
-}
--- Modulo1_UFCD_0804/Algoritmos/Pseudocodigos/b.cpp
+++
@@ -1,9 +0,0 @@
-// curso.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
-#include <iostream>
-
-int bbb()
-{
-    std::cout << "aola!\n";
-    return 0;
-}
